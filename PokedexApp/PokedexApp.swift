e7a4f34f85b1e84106507e9e18d835d259b42388
Rhys Morgan <rhysm@me.com>
2024-01-12 17:47:38 +0000

    Initial Commit

---
+++ PokedexApp/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  PokedexApp
+//
+//  Created by Rhys Morgan on 12/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
