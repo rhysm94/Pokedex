9ac5b2685524df3ee2c84a496f694f82be5ae511
Rhys Morgan <rhysm@me.com>
2024-01-12 20:08:54 +0000

    Adds a top-level reducer for the app

--- Pokedex/Sources/Pokedex/Pokedex.swift
+++ Pokedex/Sources/Pokedex/Pokedex.swift
@@ -1,2 +1,25 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  Pokedex.swift
+//  Pokedex
+//
+//  Created by Rhys Morgan on 12/01/2024.
+//
+
+import ComposableArchitecture
+
+@Reducer
+public struct Pokedex {
+  public struct State: Equatable {
+    public init() {}
+  }
+
+  public enum Action {
+
+  }
+
+  public init() {}
+
+  public var body: some ReducerOf<Self> {
+    EmptyReducer()
+  }
+}
