821d738511fbe0e28db681dbdb72690e67029d21
Rhys Morgan <rhysm@me.com>
2024-01-12 17:55:17 +0000

    Ensures the project compiles

--- Pokedex/Package.swift
+++ Pokedex/Package.swift
@@ -6,18 +6,18 @@ import PackageDescription
 let package = Package(
   name: "Pokedex",
   products: [
-    // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "Pokedex",
-      targets: ["Pokedex"]),
+      targets: ["Pokedex"]
+    ),
   ],
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
     .target(
-      name: "Pokedex"),
+      name: "Pokedex"
+    ),
     .testTarget(
       name: "PokedexTests",
-      dependencies: ["Pokedex"]),
+      dependencies: ["Pokedex"]
+    ),
   ]
 )
